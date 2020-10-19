a2488c55489b54a36324a7f69df0fdb5575b3cae
dungnguyen00018 <dungnguyen00018@gmail.com>
2020-10-19 14:35:17 +0700

    feature login

--- index.js
+++ index.js
@@ -3,37 +3,58 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const passport = require('passport');
+const cookieSession = require('cookie-session')
 
 require('./passport-setup')
 
+app.use(cookieSession({
+  name: 'tuto-session',
+  keys: ['key1', 'key2']
+}))
+
+//middleware
+const isLoginIn = (req, res, next) =>{
+  if(req.user){
+    next();
+  } else {
+    res.sendStatus(401)
+  }
+}
 
 // set view engine 
 app.set('view engine', 'ejs')
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-
 //main
 app.get('/', (req, res) => {
   res.render('pages/index')
 })
 //user succes login
-app.get('/users', (req, res) =>{
-    res.render('pages/user.ejs')
+app.get('/user', isLoginIn, (req, res) =>{
+
+    // res.send(`${req.user.displayName}`);
+    res.render('pages/user.ejs', {name: req.user.displayName})
 })
+// user login fail
+app.get('/failed', (req, res) =>{
+  res.send('Login failed')
+})
+//logout
+app.get('/logout', (req, res) =>{
+  req.session=null;
+  req.logout();
+  res.redirect('/')
+})
+
 // login 
 app.get('/login', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-// user
 
-
-// hanle affter login success
+// handle affter login success
 app.get('/login/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
   function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/users');
-    
+    res.redirect('/user');
   })
   
 app.listen(port, () => {
--- passport-setup.js
+++ passport-setup.js
@@ -3,20 +3,11 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
 passport.serializeUser(function(user, done) {
-    /*
-    From the user take just the id (to minimize the cookie size) and just pass the id of the user
-    to the done callback
-    PS: You dont have to do it like this its just usually done like this
-    */
     done(null, user);
   });
   
 passport.deserializeUser(function(user, done) {
-    /*
-    Instead of user this function usually recives the id 
-    then you use the id to select the user from the db and pass the user obj to the done callback
-    PS: You can later access this data in any routes in: req.user
-    */
+    
     done(null, user);
 });
 passport.use(new GoogleStrategy({
@@ -24,10 +15,8 @@ passport.use(new GoogleStrategy({
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: 'http://localhost:3000/login/callback',
     passReqToCallback: true
-}, (accessToken,refreshToken, profile,done)=>{
-    // console.log(accessToken);
-    // console.log(refreshToken);
-    console.log(profile);
-    //  done(null, profile)
-}
+},  function(request, accessToken, refreshToken, profile, done) {
+    // console.log(profile)
+    return done(null, profile);
+  }
 ))
